81bc61962645db5345217e8763036b503f818ece
VRolich <lynf.88@gmail.com>
2019-08-01 11:24:42 +0300

    Update unittests

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,6 +1,7 @@
 """Module Tests for Executors"""
 
 import unittest
+from unittest import mock
 
 from parameterized import parameterized
 from executors import executor
@@ -10,9 +11,31 @@ from parsers import df_parser, dfh_parser, dfi_parser
 class TestExecutor(unittest.TestCase):
     """Test executor functions"""
 
-    @parameterized.expand((('df', df_parser.DFParser),
-                           ('df -h', dfh_parser.DFHParser),
-                           ('df -i', dfi_parser.DFIParser)))
-    def test_receive_data(self, command, parser):
-        test_executor = executor.Executor(command, parser)
-        self.assertIsNotNone(test_executor.receive_data())
+    @parameterized.expand(((df_parser.DFParser,
+                            (b'Filesystem     1K-blocks     Used Available Use% Mounted on\n'
+                             b'udev             4062048        0   4062048   0% /dev'),
+                            b'',
+                            ('{"status": "success", "result": {"Filesystem": ["udev"], "1K-blocks": ["4062048"], '
+                             '"Used": ["0"], "Available": ["4062048"], "Use%": ["0%"], "Mounted on": ["/dev"]}}')),
+                           (dfh_parser.DFHParser,
+                            (b'Filesystem      Size  Used Avail Use% Mounted on\n'
+                             b'udev            3,9G     0  3,9G   0% /dev'),
+                            b'',
+                            ('{"status": "success", "result": {"Filesystem": ["udev"], "Size": ["3,9G"], '
+                             '"Used": ["0"], "Avail": ["3,9G"], "Use%": ["0%"], "Mounted on": ["/dev"]}}')),
+                           (dfi_parser.DFIParser,
+                            (b'Filesystem      Inodes  IUsed   IFree IUse% Mounted on\n'
+                             b'udev           1015512    470 1015042    1% /dev'),
+                            b'',
+                            ('{"status": "success", "result": {"Filesystem": ["udev"], "Inodes": ["1015512"], '
+                             '"IUsed": ["470"], "IFree": ["1015042"], "IUse%": ["1%"], "Mounted on": ["/dev"]}}'))))
+    @mock.patch('subprocess.Popen')
+    def test_receive_data(self, parser, stdout, stderr, expected, popen_mock):
+        p_mock = popen_mock.return_value
+        p_mock.communicate.return_value = stdout, stderr
+        p_mock.returncode = 0
+
+        actual = executor.Executor('test_cmd', parser).receive_data()
+        self.assertEqual(expected, actual, actual)
+        popen_mock.assert_called_once()
+        p_mock.assert_not_called()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -21,26 +21,6 @@ class TestParser(unittest.TestCase):
             b'tmpfs               5120        4      5116   1% /run/lock\n'\
             b'tmpfs            4084140        0   4084140   0% /sys/fs/cgroup\n'\
             b'/dev/loop0          3840     3840         0 100% /snap/gnome-system-monitor/100\n'\
-            b'/dev/loop1         55680    55680         0 100% /snap/core18/1055\n'\
-            b'/dev/loop2         15104    15104         0 100% /snap/gnome-characters/296\n'\
-            b'/dev/loop3          2432     2432         0 100% /snap/gnome-calculator/180\n'\
-            b'/dev/loop4         99840    99840         0 100% /snap/docker/384\n'\
-            b'/dev/loop16       144128   144128         0 100% /snap/gnome-3-26-1604/88\n'\
-            b'/dev/loop6         14848    14848         0 100% /snap/gnome-logs/37\n'\
-            b'/dev/loop8         43904    43904         0 100% /snap/gtk-common-themes/1313\n'\
-            b'/dev/loop9         90624    90624         0 100% /snap/core/6964\n'\
-            b'/dev/loop12       144128   144128         0 100% /snap/gnome-3-26-1604/90\n'\
-            b'/dev/loop14         2304     2304         0 100% /snap/gnome-calculator/260\n'\
-            b'/dev/loop13         4224     4224         0 100% /snap/gnome-calculator/406\n'\
-            b'/dev/loop15        14976    14976         0 100% /snap/gnome-logs/45\n'\
-            b'/dev/loop11        99840    99840         0 100% /snap/docker/321\n'\
-            b'/dev/loop17        36224    36224         0 100% /snap/gtk-common-themes/1198\n'\
-            b'/dev/loop18         3840     3840         0 100% /snap/gnome-system-monitor/95\n'\
-            b'/dev/loop19         1024     1024         0 100% /snap/gnome-logs/61\n'\
-            b'/dev/loop20       153600   153600         0 100% /snap/gnome-3-28-1804/63\n'\
-            b'/dev/loop21        15104    15104         0 100% /snap/gnome-characters/292\n'\
-            b'/dev/loop5        153600   153600         0 100% /snap/gnome-3-28-1804/67\n'\
-            b'/dev/loop10        90624    90624         0 100% /snap/core/7270\n'\
             b'tmpfs             816828       36    816792   1% /run/user/1000\n'\
             b'/dev/loop22        55808    55808         0 100% /snap/core18/1066\n'\
 
@@ -50,61 +30,26 @@ class TestParser(unittest.TestCase):
         cls._dfi_parser_object = dfi_parser.DFIParser(cls._byte_input, '', 0)
 
         cls._parsed_dict = {
-            'Filesystem': ['udev', 'tmpfs', '/dev/sda1', 'tmpfs', 'tmpfs', 'tmpfs', '/dev/loop0', '/dev/loop1',
-                           '/dev/loop2', '/dev/loop3', '/dev/loop4', '/dev/loop16', '/dev/loop6', '/dev/loop8',
-                           '/dev/loop9', '/dev/loop12', '/dev/loop14', '/dev/loop13', '/dev/loop15', '/dev/loop11',
-                           '/dev/loop17', '/dev/loop18', '/dev/loop19', '/dev/loop20', '/dev/loop21', '/dev/loop5',
-                           '/dev/loop10', 'tmpfs', '/dev/loop22'],
-            '1K-blocks': ['4062048', '816832', '51341792', '4084140', '5120', '4084140', '3840', '55680', '15104',
-                          '2432', '99840', '144128', '14848', '43904', '90624', '144128', '2304', '4224', '14976',
-                          '99840', '36224', '3840', '1024', '153600', '15104', '153600', '90624', '816828', '55808'],
-            'Used': ['0', '1404', '26827224', '69784', '4', '0', '3840', '55680', '15104', '2432', '99840', '144128',
-                     '14848', '43904', '90624', '144128', '2304', '4224', '14976', '99840', '36224', '3840', '1024',
-                     '153600', '15104', '153600', '90624', '36', '55808'],
-            'Available': ['4062048', '815428', '21876848', '4014356', '5116', '4084140', '0', '0', '0', '0', '0', '0',
-                          '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '816792', '0'],
-            'Use%': ['0%', '1%', '56%', '2%', '1%', '0%', '100%', '100%', '100%', '100%', '100%', '100%', '100%',
-                     '100%', '100%', '100%', '100%', '100%', '100%', '100%', '100%', '100%', '100%', '100%', '100%',
-                     '100%', '100%', '1%', '100%'],
+            'Filesystem': ['udev', 'tmpfs', '/dev/sda1', 'tmpfs', 'tmpfs', 'tmpfs', '/dev/loop0', 'tmpfs',
+                           '/dev/loop22'],
+            '1K-blocks': ['4062048', '816832', '51341792', '4084140', '5120', '4084140', '3840', '816828', '55808'],
+            'Used': ['0', '1404', '26827224', '69784', '4', '0', '3840', '36', '55808'],
+            'Available': ['4062048', '815428', '21876848', '4014356', '5116', '4084140', '0', '816792', '0'],
+            'Use%': ['0%', '1%', '56%', '2%', '1%', '0%', '100%', '1%', '100%'],
             'Mounted on': ['/dev', '/run', '/', '/dev/shm', '/run/lock', '/sys/fs/cgroup',
-                           '/snap/gnome-system-monitor/100', '/snap/core18/1055', '/snap/gnome-characters/296',
-                           '/snap/gnome-calculator/180', '/snap/docker/384', '/snap/gnome-3-26-1604/88',
-                           '/snap/gnome-logs/37', '/snap/gtk-common-themes/1313', '/snap/core/6964',
-                           '/snap/gnome-3-26-1604/90', '/snap/gnome-calculator/260', '/snap/gnome-calculator/406',
-                           '/snap/gnome-logs/45', '/snap/docker/321', '/snap/gtk-common-themes/1198',
-                           '/snap/gnome-system-monitor/95', '/snap/gnome-logs/61', '/snap/gnome-3-28-1804/63',
-                           '/snap/gnome-characters/292', '/snap/gnome-3-28-1804/67', '/snap/core/7270',
-                           '/run/user/1000', '/snap/core18/1066']}
+                           '/snap/gnome-system-monitor/100', '/run/user/1000', '/snap/core18/1066']}
 
         cls._result_dict = {
             "status": "success", "result": {
-                "Filesystem": ["udev", "tmpfs", "/dev/sda1", "tmpfs", "tmpfs", "tmpfs", "/dev/loop0", "/dev/loop1",
-                               "/dev/loop2", "/dev/loop3", "/dev/loop4", "/dev/loop16", "/dev/loop6", "/dev/loop8",
-                               "/dev/loop9", "/dev/loop12", "/dev/loop14", "/dev/loop13", "/dev/loop15", "/dev/loop11",
-                               "/dev/loop17", "/dev/loop18", "/dev/loop19", "/dev/loop20", "/dev/loop21", "/dev/loop5",
-                               "/dev/loop10", "tmpfs", "/dev/loop22"],
-                "1K-blocks": ["4062048", "816832", "51341792", "4084140", "5120", "4084140", "3840", "55680", "15104",
-                              "2432", "99840", "144128", "14848", "43904", "90624", "144128", "2304", "4224", "14976",
-                              "99840", "36224", "3840", "1024", "153600", "15104", "153600", "90624", "816828",
+                "Filesystem": ["udev", "tmpfs", "/dev/sda1", "tmpfs", "tmpfs", "tmpfs", "/dev/loop0", "tmpfs",
+                               "/dev/loop22"],
+                "1K-blocks": ["4062048", "816832", "51341792", "4084140", "5120", "4084140", "3840", "816828",
                               "55808"],
-                "Used": ["0", "1404", "26827224", "69784", "4", "0", "3840", "55680", "15104", "2432", "99840",
-                         "144128", "14848", "43904", "90624", "144128", "2304", "4224", "14976", "99840", "36224",
-                         "3840", "1024", "153600", "15104", "153600", "90624", "36", "55808"],
-                "Available": ["4062048", "815428", "21876848", "4014356", "5116", "4084140", "0", "0", "0", "0", "0",
-                              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "816792",
-                              "0"],
-                "Use%": ["0%", "1%", "56%", "2%", "1%", "0%", "100%", "100%", "100%", "100%", "100%", "100%", "100%",
-                         "100%", "100%", "100%", "100%", "100%", "100%", "100%", "100%", "100%", "100%", "100%", "100%",
-                         "100%", "100%", "1%", "100%"],
+                "Used": ["0", "1404", "26827224", "69784", "4", "0", "3840", "36", "55808"],
+                "Available": ["4062048", "815428", "21876848", "4014356", "5116", "4084140", "0", "816792", "0"],
+                "Use%": ["0%", "1%", "56%", "2%", "1%", "0%", "100%", "1%", "100%"],
                 "Mounted on": ["/dev", "/run", "/", "/dev/shm", "/run/lock", "/sys/fs/cgroup",
-                               "/snap/gnome-system-monitor/100", "/snap/core18/1055", "/snap/gnome-characters/296",
-                               "/snap/gnome-calculator/180", "/snap/docker/384", "/snap/gnome-3-26-1604/88",
-                               "/snap/gnome-logs/37", "/snap/gtk-common-themes/1313", "/snap/core/6964",
-                               "/snap/gnome-3-26-1604/90", "/snap/gnome-calculator/260", "/snap/gnome-calculator/406",
-                               "/snap/gnome-logs/45", "/snap/docker/321", "/snap/gtk-common-themes/1198",
-                               "/snap/gnome-system-monitor/95", "/snap/gnome-logs/61", "/snap/gnome-3-28-1804/63",
-                               "/snap/gnome-characters/292", "/snap/gnome-3-28-1804/67", "/snap/core/7270",
-                               "/run/user/1000", "/snap/core18/1066"]}}
+                               "/snap/gnome-system-monitor/100", "/run/user/1000", "/snap/core18/1066"]}}
 
     @parameterized.expand([([['first', 'line'], ['second', 'line'], ['third', 'line']],
                             b'first line\nsecond line\nthird line')])
